1bfc28d4220575dbe2fdc7b81a483f860aa55979
Christopher Escalon <escalonc@users.noreply.github.com>
2025-04-05 09:56:35 -0600

    Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import { esMX } from "@clerk/localizations";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
